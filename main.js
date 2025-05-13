80a47e2e0ac4dff7329ab3cd756cc502a7555160
dontai cantrell jr <98494230+poiut23@users.noreply.github.com>
2025-05-13 09:42:43 -0500

    Update HTML structure and fix JavaScript background color assignment

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var main = function () {
     const first = document.body.children[0];
     first.innerHTML = "BROWN BEARS ARE AWESOME!";
 
-    first.parentNode.style.backgroundColor = "beige";
+    first.parentNode.style.backgroundColor = "beige"
+    
 
 
 
